6a15484ba8f29e006fa6e142553dc3fb9829e4b0
alright21 <alberto.giust@studenti.unitn.it>
2021-05-25 15:57:15 +0300

    feat(main): add v1.0, scan port 80

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
 func main(){
 
+	_, scanError := net.Dial("tcp", "scanme.nmap.org:80")
+
+	if scanError != nil {
+		log.Fatal("Scan Error: ", scanError)
+	}
+
+	fmt.Println("Connection successful")
 }
